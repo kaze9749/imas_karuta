d6e289c7f0de927b25a0f37bb60bbc11a3b4e1e4
shuheikoshida <shuheikoshida@gmail.com>
2025-03-30 13:17:57 +0900

    rennameしてrebuild

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ImasKaruta msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImasKaruta from './components/ImasKaruta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImasKaruta
   }
 }
 </script>
